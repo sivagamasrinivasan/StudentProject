b2ac4ddac9bc95fad673afaec6d92bb66d86dd8f
sivagamasrinivasan <784108@pdsb.net>
2024-05-13 17:36:05 -0400

    version 2 of student prj

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -12,6 +12,7 @@ public class Student
 {
     private String name; // encapsulation 
    private  int age;
+   private int sid;
 
     /**
      * @return the name
@@ -39,5 +40,19 @@ public class Student
      */
     public void setAge(int age) {
         this.age = age;
+    }
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
     }
 }
